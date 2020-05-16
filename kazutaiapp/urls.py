e766e0e943b7b19cff627f507b32896437a72ab8
Ka-zyuma <63433423+Ka-zyuma@users.noreply.github.com>
2020-05-16 19:43:21 +0900

    Update urls.py

--- kazutaiapp/urls.py
+++ kazutaiapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 from .views import InfoRegister
 urlpatterns = [
-    path('info/', views.info, name='info' ),
+    path('index/', views.info, name='info' ),
     path('info/register/', InfoRegister.as_view(), name='register' ),
 ]
